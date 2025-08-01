1bc1d0b36f8438027ccdf97ec161702e42583a41
bhaynes32 <145616141+bhaynes32@users.noreply.github.com>
2025-08-01 14:50:49 -0400

    Update final.py

--- final.py
+++ final.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import io
 from scipy.stats import beta
-import xlsxwriter
 
 st.title("Schedule RESP Analyzer")
 
@@ -134,16 +133,18 @@ if uploaded_file:
         simulated_df = estimate_on_time_probabilities(result_df)
         st.write("🎯 Probability of Finishing On Time (based on Monte Carlo with betaPERT distribution):")
         st.dataframe(simulated_df)
-        
+
+        """
         # Download simulation results
         output = io.BytesIO()
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
             simulated_df.to_excel(writer, index=False)
         st.download_button("Download Simulation Results", output.getvalue(), file_name="Resp-Simulation.xlsx")
-        
+        """
 
     else:
         st.warning("No valid RESP data found in uploaded file.")
+
 
 
 
